daf619e33233e9358d82dd5e4b48731b81676263
Victo Almeida <victorres.almeida@gmail.com>
2022-05-05 11:41:14 +0100

    sorted list of wines by type

--- src/Components/Wine.js
+++ src/Components/Wine.js
@@ -1,27 +1,31 @@
 import wineList from "./List.js";
 
+let sortedList = wineList.sort(function (a, b) {
+    return a.tipo.localeCompare(b.tipo)
+})
+
 export function Wine() {
 
-    const filteredWines = wineList.map((item) => (
+    const filteredWines = sortedList.map((item) => (
         <div className="row">
-               <p> {item.tipo} &emsp; 
-               
-               <b>{item.vinho} </b> &ensp; 
-               {item.produtor} &ensp;         
-           
+            <p> {item.tipo} &emsp;
+
+                <b>{item.vinho} </b> &ensp;
+                {item.produtor} &ensp;
+
                 {item.regiao} &ensp;
-            
+
                 {item.pais} &ensp;
-           
+
                 {item.ano} &ensp;
-                </p>
+            </p>
         </div >
 
     ))
     return (
-        
 
-        
+
+
         <div className="p-4">
             <h4> Estilo  &emsp;  Vinho &emsp; Produtor &emsp; Região &emsp; País  &emsp; Ano </h4>
             <p>{filteredWines}</p>
